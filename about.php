5453209aa93312ec65a29378f1fcbf7ed5037c62
Filip Kubina <fkubina@gmail.com>
2015-12-26 22:39:14 +0100

    Update about.php

--- about.php
+++ about.php
@@ -12,7 +12,96 @@
 
     <!-- let the content begin -->
 
-
+    <h1>Čo je Nôdľa?</h1>
+    <p>
+    Nôdľa je tímová šifrovacia hra pre dospelých. 
+    Štvorčlenný tím sa stretne na štarte s nádejou, že o pár hodín neskôr práve im v cieli zagratulujeme k víťazstvu. 
+    Spolu s nimi štartuje kopa (100-200) ďalších ľudí. 
+    Po polhodine od štartu sú.. ešte na štarte. Nevadí, sú tam aj ostatní. Nakoniec sa im podarí opustiť štart a vydajú sa do lesov.
+    O pár hodín neskôr zistia, že asi nevyhrajú a ešte neskôr, že vlastne možno nestihnú prísť ani do cieľa. To však vôbec nevadí.
+    Šifrovacie hry sú totiž hlavne o zážitkoch z hry. A je to výzva a je skvelý pocit ju zdolať.
+    </p>
+    <p>
+    Ale späť k hre. Na štarte dostanú šifru. Vyriešia šifru. Riešením je popis, kam majú ísť ďalej. 
+    Tam nájdu ďalšiu šifru. Opäť vyriešia. A tak ďalej. Šifier je približne 11. Vyzerá to ľahko? Nie je! 
+    To by predsa nebol zážitok, keby na obed už boli všetky tímy v cieli. Treba prekonať dve prekážky. Fyzickú. 
+    Prejsť 15km za 12h nie je veľa, ale netreba zabúdať, že je to po teréne, je zima, tím sa môže stratiť,
+    môže sa brodiť snehom a večer bude tma.
+    A psychickú. Šifry nie sú ľahké. Ani príliš ťažké. Sú také, aby vás potrápili a mali ste dobrý pocit z ich vyriešenia.
+    </p>
+    <p>
+    Šifry sú všeliaké. Zväčša je to papier. S textom či s obrázkom. No niekedy to môže byť niečo úplne nečakané. Napríklad bageta. 
+    Vašou úlohou je zistiť, kam pokračovať ďalej. Nik iný vám to nepovie. 
+    Ale nebojte sa, nenecháme vás umrznúť v lese :)
+    </p>
+    <!--
+    <p>
+    Nôdľu organizujú ľudia z tímu Ešte sa dohodneme, ktorý sa rád zúčastňuje podobných akcií hlavne v ČR. 
+    Ak chcete vidieť, ako to vyzerá na štarte legendárnej TMOU, tu je <a href="https://www.youtube.com/watch?v=z6Fcw-7zlnA" target="_blank">motivačné video</a>.
+    </p>
+    -->
+    
+    <h2>Ako riešiť šifry?</h2>
+    <p>
+    Kam teda? Šifra nie je kvízová otázka, ktorú buď viete alebo neviete zodpovedať. Šifru na prvý pohľad NEVIETE vyriešiť. 
+    Neexistuje ani databáza riešení. Každá šifra je originálna. Treba sa zamyslieť. 
+    Nie je problém vymyslieť neriešiteľnú šifru. Tu je: fwuagughrkjs. My však chceme, aby šifra bola riešiteľná. 
+    A aby ste mali z nej zážitok. Budete mať zážitok z prvočíselného rozkladu obrovských čísiel? Asi nie. To je práca pre banky a počítače. 
+    Takú šifru vám nedáme.
+    </p>
+    <p>
+    Od šifry teda treba očakávať, že bude nejakým spôsobom zaujímavá a pekná, a že bude jasné, že ste ju vyriešili správne. 
+    Dostali ste bagetu. Mohli by ste chcieť ísť do pekárne. Alebo do obchodu?
+    Naozaj si myslíte, že rok vymýšľame šifrovačku kvôli tomu aby ste náhodne obiehali blízke prevádzky? 
+    Dobrá šifra je jednoznačná a keď ju vyriešite viete, že ste ju vyriešili správne. Keby bola bageta zabalená do francúzskej vlajky
+    a pekáreň by bola na Francúzskej ulici, tak by to vyzeralo nádejne. Asi to nebude náhoda. Každý detail môže byť dôležitý. 
+    Pravdepodobne nebudeme baliť bagetu do vlajky, ak by tá vlajka nebola potrebná k riešeniu. 
+    Ale keďže vy ste dostali len bagetu, treba sa radšej zamyslieť. Čo všetko ste dostali? Čo je zadanie? 
+    A analyzovať a pýtať sa prečo. Analýza je najdôležitejšia časť. 
+    Dostanem farebný obrázok, ale očividne chýba na ňom modrá farba. Prečo? Mám kopu čísiel, ale všetky sú párne. Prečo? 
+    Prečo každá veta začína iným písmenom? Prečo všetky uhly sú pravé? Prečo sme si mali zobrať zápalky?
+    Prečo je obrázkov 26 - toľko, ako písmen anglickej abecedy, ktorá sa používa na šifrovačkách?
+    Tieto náhody zväčša nie sú náhody. Správna odpoveď často vedie k riešeniu. 
+    </p>
+    <p> 
+    My máme teraz bagetu. Čo s ňou? 
+    Môžete ju odvážiť a premerať, ale ochutnaniu sa asi nevyhnete. A zrazu zistíte, že je plnená. 
+    Vnútri sú nejaké zapečené guličky z cesta.
+    Tak ich predsa vyberte! Dávajte pozor, čo robíte. Ich poradie môže byť dôležité. 
+    A ak nejakú stratíte alebo zjete, tak šifru asi ľahko nevyriešite... No dobre, máte očíslované pečené guličky. Čo s nimi? 
+    Áno, asi opäť ochutnať. Tá prvá chutí ako hrach? Fuj. Druhá.. sú to egreše? Tretia slivky? Štvrtá citrón. Piata oregano. 
+    A píše si to niekto na papier? Lebo len tak z hlavy to asi nevyriešite... Čím skúsenejší hráč, tým viac si píše. 
+    Na papieri majte toto: hrach, egreše, slivky, citrón, oregano. V správnom poradí!
+    Čo by sa dalo robiť s týmito slovami? No napríklad prečítať prvé písmená. HESCO. Hesco? Nebude to náhodou heslo? 
+    Ahá, to nebude citrón, ale limetka! Toto je ukážka aj toho, že je fajn kontrolovať veci po sebe.
+    </p>
+    <p>
+    V skutočnej šifre by vám vyšlo miesto 'HESLO' napríklad 'HODZOVONAMFONTANA'. Hodžovo námestie, šifra bude pri fontáne. Hurá, môžete ísť :) 
+    Ani sa nenazdáte a na takejto šifre môžete stráviť hodinu, kým vám napadú všetky potrebné kroky :) 
+    Panikáriť a vzdávať sa po piatich minútach nie je na mieste.
+    </p>
+    <>TU VLOZ FOTKU MILKY S BAGETOU<>
+    <p>
+    Ak chcete potrénovať a pripraviť sa na hru, môžete si pozrieť napríklad stránky skvelej letnej Bratislavskej šifrovačky <a href="http://sifrovacka.sk/" target="_blank">Brieždenie</a>.
+    Databáza množstva (českých) hier je <a href="http://kalendar.chlyftym.cz/" target="_blank">tu</a>.
+    </p>
+    
+    <p>
+    Šifrovačky zväčša používajú anglickú 26 znakovú abecedu a prevod medzi číslami a písmenami A=1, B=2, tj 8,5,19,12,15 = HESLO.
+    Doporučujeme sa zoznámiť so šifrovacími pomôckami, ktoré sú medzi doporučeným vybavením na stránke s informáciami a pravidlami.
+    Na šifrovačke síce takmer nikdy nenájdete morzeovku či braila v bežnej forme, ale existujú tisíce spôsobov, ako ich využiť skryto. 
+    Listnatý strom, ihličnan, trávnik? No predsa krátka, dlhá, oddeľovač :-) Netreba vedieť morzeovku naspamäť - tú máte v pomôckach. 
+    Treba nájsť tú morzeovku skrytú v šifre.
+    </p>
+  
+    Prajeme vám kopu zážitkov!
+    <!--
+    Matfyzáci, jazykovedci, klaviristi, orientační bežci, právnici. Je dobré mať rôznorodý tím, každý vie totiž čosi iné. 
+    K šifrovaniu netreba žiadne špeciálne vedomosti. Internet je predsa k dispozícii. Dôležité sú schopnosti. 
+    Je fajn, keď niekto vie pracovať s mapou, niekto udrží morálku v tíme, niekto sa nevzdá, ani keď sa nedarí, 
+    niekto prichádza s bláznivými nápadmi.
+    </p>
+    -->
 
     <?php include "footer.php" ?>
     <!-- jQuery -->
