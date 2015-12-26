9b7b1644d2a35088ef042ca4fe884bcf8a1adffb
Filip Kubina <fkubina@gmail.com>
2015-12-26 20:23:43 +0100

    Update competition_vars.php

--- competition_vars.php
+++ competition_vars.php
@@ -1,5 +1,5 @@
 <?php
 $competitionstart = strtotime("February 20, 2016 9:00 AM");
-$registrationend = strtotime("February 14, 2016 24:00 PM");
+$registrationend = strtotime("February 14, 2016 12:00 PM");
 $capacity = 40;
 ?>
