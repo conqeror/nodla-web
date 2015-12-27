bec51ddf9782d84361d235ee8a67fa0bf062c38d
Samuel Cibulka <samuel.cibulka@gmail.com>
2015-12-27 11:27:59 +0100

    Added fade effect to tabs in rules

--- rules.php
+++ rules.php
@@ -30,7 +30,7 @@
         <!-- Content -->
         <div class="col-md-8">
           <div class="tab-content">
-            <div class="tab-pane active" id="info"><h2>Dôležité informácie</h2>
+            <div class="tab-pane fade in active" id="info"><h2>Dôležité informácie</h2>
           <ul>
       		<li>Štvrtý ročník šifrovacej hry Nôdľa sa uskutoční v <strong>sobotu 20. februára 2016.</strong></li>
       		<li>Nôdľa bude prebiehať cez deň <strong>od 9:00 do 21:00</strong>. </li>
@@ -40,7 +40,7 @@
       		<li>Obtiažnosť šifier na Nôdli by nemala byť príliš veľká. Očakávame, že aj šikovné začiatočnícke tímy nezamrznú (v akomkoľvek význame) niekde na začiatku.</li>
       		<li>Napriek tomu aj skúsené tímy budú mať kopu zážitkov, viď šifry a fotky z minulého ročníka.</li>
       		<li>Úplní začiatočníci môžu prísť tiež, dajte nám vedieť a budeme k vám extra milí :-)</li></div>
-            <div class="tab-pane" id="signup"><h2>Prihlasovanie a platba štartovného</h2>
+            <div class="tab-pane fade" id="signup"><h2>Prihlasovanie a platba štartovného</h2>
             		<p>
             		Prihlasovanie je otvorené do 14.2. (prihláška je ideálny Valentínsky darček ;-), prihlasovanie po tomto termíne je možné po konzultácii s organizátormi.
             		Čím skôr sa však prihlásite, tým lepšie pre nás - objednávame nejaké veci a neskoré prihlášky nám to komplikujú. Ďakujeme.
@@ -52,7 +52,7 @@
                 </p>
                 <p><b>Hry sa zúčastňujete na vlastnú zodpovednosť. Minimálny vek je preto 18 rokov.</b></p>
             </div>
-            <div class="tab-pane" id="rules"><h2>Pravidlá</h2>
+            <div class="tab-pane fade" id="rules"><h2>Pravidlá</h2>
                 <p>V princípe platia bežné šifrovačkové pravidlá. Tu je zopár pravidiel,
                 ktoré chceme zdôrazniť alebo sa mierne líšia od iných šifrovačiek.</p>
                 <ul>
@@ -81,7 +81,7 @@
                 V prípade, že sa rozhodnete hru predčasne ukončiť, kontaktujte nás.
                 Jednak chceme mať prehľad, kto kde je, dvak bola by škoda, keby ste to vzdali tesne pred nejakým skvelým stanoviskom, no nie?
                 </p></div>
-            <div class="tab-pane" id="points"><h2>Bodovanie a nápovedy</h2>
+            <div class="tab-pane fade" id="points"><h2>Bodovanie a nápovedy</h2>
                 <p>Vyhráva tím s najvyšším počtom bodov, v prípade rovnosti rozhoduje najvyššie navštívené stanovisko a čas jeho dosiahnutia. Za vyriešenie cieľovej aktivity je +1b.
                 Ku každej šifre existujú 2 nápovedy a totálka. Použitie jednej nápovedy je penalizované 0.5b, dvoch dokopy 0.75b. Tieto body sa strhnú len v prípade, že prídete na nasledujúce stanovisko.
                 Za regulárne vyriešenie šifry (aj s využitím nápoved) a príchod na nasledujúce stanovisko je +1b.
@@ -89,7 +89,7 @@
 
                 <p>Nápovedy a totálky si pýtajte od orga na stanovisku. V prípade, že stanovisko orga nemá, zavolajte na telefónne číslo uvedené v posledných informáciách alebo na štarte.</p>
               </div>
-            <div class="tab-pane" id="bring"><h2>Čo si doniesť</h2>
+            <div class="tab-pane fade" id="bring"><h2>Čo si doniesť</h2>
                 <ul>
                     <li>potrebnú mapu dostanete na štarte, vlastné turistické a online mapy sa môžu hodiť, ale používate ich na vlastnú zodpovednosť</li>
                     <li>čelovky - bude tma</li>
@@ -103,7 +103,7 @@
                 </ul>
                 Zoznam sa môže ešte meniť s blížiacim sa dátumom hry :)
               </div>
-            <div class="tab-pane" id="lastinfo"><h2> Posledné informácie </h2>
+            <div class="tab-pane fade" id="lastinfo"><h2> Posledné informácie </h2>
               <p>Čakajte, objavia sa neskôr na tomto mieste.</p>
             </div>
           </div>
