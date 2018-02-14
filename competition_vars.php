6aa152a66533bd0ee4ed80576727aea69d5096c1
Samuel Cibulka <samuel.cibulka@gmail.com>
2018-02-14 14:20:53 +0100

    Update competition vars.

--- competition_vars.php
+++ competition_vars.php
@@ -1,5 +1,5 @@
 <?php
-$competitionstart = strtotime("February 20, 2016 9:00 AM");
-$registrationend = strtotime("February 14, 2016 12:00 PM");
+$competitionstart = strtotime("March 1, 2018 9:00 AM");
+$registrationend = strtotime("April 24, 2018 12:00 PM");
 $capacity = 40;
 ?>
