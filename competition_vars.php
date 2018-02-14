e7ac9c0c18e6f2d4b1a05b2c2a9c2de350bb024d
Samuel Cibulka <samuel.cibulka@gmail.com>
2018-02-14 14:24:01 +0100

    Fix vars.

--- competition_vars.php
+++ competition_vars.php
@@ -1,5 +1,5 @@
 <?php
-$competitionstart = strtotime("March 1, 2018 9:00 AM");
+$competitionstart = strtotime("April 27, 2018 17:00 PM");
 $registrationend = strtotime("April 24, 2018 12:00 PM");
 $capacity = 40;
 ?>
