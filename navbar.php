ac00e45a5d03b111b3dc023b6b115f81027d91d5
Samuel Cibulka <samuel.cibulka@gmail.com>
2018-02-14 14:15:04 +0100

    Uncomment db things in master.

--- navbar.php
+++ navbar.php
@@ -14,14 +14,12 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <!--
                     <li>
                         <a href="news.php">Aktuality</a>
                     </li>
                     <li>
                         <a href="results.php">Výsledky</a>
                     </li>
-                    -->
                     <li>
                         <a href="ciphers.php">Šifry</a>
                     </li>
@@ -31,7 +29,6 @@
                     <li>
                         <a href="rules.php">Info a Pravidlá</a>
                     </li>
-                    <!--
                     <li>
                         <a href="register.php">Registrácia</a>
                     </li>
@@ -41,7 +38,6 @@
                     <li>
                         <a href="forum.php">Fórum</a>
                     </li>
-                    -->
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
